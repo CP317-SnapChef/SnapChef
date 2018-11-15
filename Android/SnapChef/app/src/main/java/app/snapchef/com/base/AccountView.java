11b833cd56650507256dda3fd3437d20541532f4
Erman <dins4040@mylaurier.ca>
2018-11-14 20:24:44 -0500

    Added bottom navigation bar. Switches between home view and account view. Currently crashes if attempt to switch to other views.

--- Android/SnapChef/app/src/main/java/app/snapchef/com/base/AccountView.java
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/AccountView.java
@@ -1,7 +1,12 @@
 package app.snapchef.com.base;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
 
 import app.snapchef.com.base.R;
 
@@ -10,7 +15,44 @@ public class AccountView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_account_view);
-        getSupportActionBar().setTitle("Account");
+       // getSupportActionBar().setTitle("Account");
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        bottomNav.getMenu().getItem(0).setChecked(true);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Intent intent = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_account:
+                    intent = new Intent(getApplicationContext(), AccountView.class);
+                    break;
+
+                case R.id.navigation_home:
+                    intent  = new Intent(getApplicationContext(), HomeView.class);
+                    break;
+
+                case R.id.navigation_search:
+                    // intent = new Intent(getApplicationContext(), .class);
+                    break;
+
+                case R.id.navigation_add:
+                    //intent = new Intent(getApplicationContext(), NewRecipeView.class);
+                    break;
+
+
+                case R.id.navigation_text_search:
+                    // intent = new Intent(getApplicationContext(), .class);
+                    break;
+            }
+
+            startActivity(intent);
+            return true;
+        }
+    };
 }
--- Android/SnapChef/app/src/main/java/app/snapchef/com/base/HomeView.java
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/HomeView.java
@@ -1,8 +1,11 @@
 package app.snapchef.com.base;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -15,14 +18,40 @@ public class HomeView extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_view);
 
-        Button  goToAccBtn = (Button) findViewById(R.id.goToAccBtn);
-        goToAccBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //intent to go to account view
-                Intent accountIntent = new Intent(getApplicationContext(), AccountView.class);
-                startActivity(accountIntent);
-            }
-            });
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        bottomNav.getMenu().getItem(2).setChecked(true);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Intent intent = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_account:
+                    intent = new Intent(getApplicationContext(), AccountView.class);
+                    break;
+
+                case R.id.navigation_home:
+                    intent  = new Intent(getApplicationContext(), HomeView.class);
+                    break;
+
+                case R.id.navigation_search:
+                    // intent = new Intent(getApplicationContext(), .class);
+                    break;
+
+                case R.id.navigation_add:
+                    //intent = new Intent(getApplicationContext(), NewRecipeView.class);
+                    break;
+
+
+                case R.id.navigation_text_search:
+                    // intent = new Intent(getApplicationContext(), .class);
+                    break;
+            }
+
+            startActivity(intent);
+            return true;
+        }
+    };
 }
