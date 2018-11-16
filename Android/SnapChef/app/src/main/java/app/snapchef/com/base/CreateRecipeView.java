144a5100a942ee1c744042ee8d530cfbcca0f4c8
StuartIsley <isle7220@mylaurier.ca>
2018-11-15 23:08:44 -0500

    Created CreateRecipeView

---
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/CreateRecipeView.java
@@ -0,0 +1,15 @@
+package app.snapchef.com.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateRecipeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+}
