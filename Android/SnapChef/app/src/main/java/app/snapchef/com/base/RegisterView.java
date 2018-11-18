c0c944778ccfaa6dc1571d1befde29014c4a0ca2
StuartIsley <isle7220@mylaurier.ca>
2018-11-17 23:39:57 -0500

    Completed RegisterView

--- Android/SnapChef/app/src/main/java/app/snapchef/com/base/RegisterView.java
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/RegisterView.java
@@ -1,8 +1,38 @@
 package app.snapchef.com.base;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RegisterView extends AppCompatActivity {
+    private ImageButton backBtn;
+    private Intent intent;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_view);
+        //press to go to home screen
+        Button mEmailSignUpButton = (Button) findViewById(R.id.registerButton);
+        mEmailSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(),HomeView.class);
+                startActivity(intent);
+            }
+        });
 
-
+        //go back to login screen if back button is pressed
+        backBtn = (ImageButton) findViewById(R.id.backButtonAccount);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent =  new Intent(getApplicationContext(), LoginView.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
