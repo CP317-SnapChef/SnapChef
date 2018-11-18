cc732fcb0d225b8eaba5595e197a59b18fc6ac8f
StuartIsley <isle7220@mylaurier.ca>
2018-11-18 13:24:31 -0500

    Started working on GUI for ViewRecipe, i dont like scroll view

--- Android/SnapChef/app/src/main/java/app/snapchef/com/base/ViewRecipeView.java
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/ViewRecipeView.java
@@ -1,14 +1,31 @@
 package app.snapchef.com.base;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ViewRecipeView extends AppCompatActivity {
     //TODO: Again... literally this entire view, but it should be similar to CreateRecipeView
+
+    private ImageButton backBtn;
+    private Intent intent;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recipe);
+
+
+        //go back to home screen if back button is pressed
+        backBtn = (ImageButton) findViewById(R.id.backButtonAccount);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent =  new Intent(getApplicationContext(), HomeView.class);
+                startActivity(intent);
+            }
+        });
     }
 }
