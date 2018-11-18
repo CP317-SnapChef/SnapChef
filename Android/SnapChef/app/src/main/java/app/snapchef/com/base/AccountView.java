938575736e55ac497affba83f559f943935106c4
Erman <dins4040@mylaurier.ca>
2018-11-17 19:59:55 -0500

    Account view design updated with working logout and back buttons.

--- Android/SnapChef/app/src/main/java/app/snapchef/com/base/AccountView.java
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/AccountView.java
@@ -6,12 +6,17 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import app.snapchef.com.base.R;
 
 public class AccountView extends AppCompatActivity {
-
+private ImageButton backBtn;
+private Button logout;
+private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +27,32 @@ public class AccountView extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         bottomNav.getMenu().getItem(0).setChecked(true);
+
+        //go back to login screen if logout button is pressed
+        logout = (Button) findViewById(R.id.logOutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent =  new Intent(getApplicationContext(), LoginView.class);
+                startActivity(intent);
+            }
+        });
+        //go back to home screen if back button is pressed
+        backBtn = (ImageButton) findViewById(R.id.backButton);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent =  new Intent(getApplicationContext(), HomeView.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Intent intent = null;
+             intent = null;
             switch (item.getItemId()) {
                 case R.id.navigation_account:
                     intent = new Intent(getApplicationContext(), AccountView.class);
