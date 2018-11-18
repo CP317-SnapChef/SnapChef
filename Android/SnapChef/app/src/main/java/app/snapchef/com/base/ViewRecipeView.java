84f08ea870700743b520669a03ba040d5bdc58ae
Erman <dins4040@mylaurier.ca>
2018-11-18 16:03:06 -0500

    Design of view recipe view updated with working report button

--- Android/SnapChef/app/src/main/java/app/snapchef/com/base/ViewRecipeView.java
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/ViewRecipeView.java
@@ -5,25 +5,36 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class ViewRecipeView extends AppCompatActivity {
     //TODO: Again... literally this entire view, but it should be similar to CreateRecipeView
 
     private ImageButton backBtn;
+    private Button reportBtn;
     private Intent intent;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recipe);
 
 
         //go back to home screen if back button is pressed
-        backBtn = (ImageButton) findViewById(R.id.backButtonAccount);
+        backBtn = (ImageButton) findViewById(R.id.backButtonViewRecipe);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 intent =  new Intent(getApplicationContext(), HomeView.class);
+                startActivity(intent);
+            }
+        });
+
+        reportBtn = (Button) findViewById(R.id.reportRecipeButtonViewRecipe);
+        reportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent =  new Intent(getApplicationContext(), ReportRecipeView.class);
                 startActivity(intent);
             }
         });
