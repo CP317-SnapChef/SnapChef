f154cde7df04b702c0ac1d7f75478eb992b4cfea
Jun Cao <43151278+juncao11@users.noreply.github.com>
2018-12-11 12:06:51 -0500

    Added test activity to test the api Added test button in home view to go to test api activity HTTP connection works

--- Android/SnapChef/app/src/main/java/app/snapchef/com/base/SearchView.java
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/SearchView.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -12,12 +13,29 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 //TODO: Add camera button
 public class SearchView extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner sortBy, filter;
     private ImageButton backBtn, cameraBtn;
     private Button viewRecipeBtn;
     private Intent intent;
+    TextView responseView;
+    static final String API_URL = "https://1y81ltee41.execute-api.us-east-1.amazonaws.com/default/BackendLambda";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +80,54 @@ public class SearchView extends AppCompatActivity implements AdapterView.OnItemS
                startActivity(intent);
             }
         });
+
+//        Button apiButton = (Button) findViewById(R.id.apiButton);
+//        apiButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                new RetrieveFeedTask().execute();
+//            }
+//        });
     }
+//
+//    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+//        private Exception exception;
+//
+//        protected void onPreExecute() {
+//            responseView.setText("");
+//        }
+//
+//        protected String doInBackground(Void... urls) {
+//            try {
+//                URL url = new URL(API_URL);
+//                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//                try {
+//                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//                    StringBuilder stringBuilder = new StringBuilder();
+//                    String line;
+//                    while ((line = bufferedReader.readLine()) != null) {
+//                        stringBuilder.append(line).append("\n");
+//                    }
+//                    bufferedReader.close();
+//                    return stringBuilder.toString();
+//                } finally {
+//                    urlConnection.disconnect();
+//                }
+//            } catch (Exception e) {
+//                Log.e("ERROR", e.getMessage(), e);
+//                return null;
+//            }
+//        }
+//    }
+//
+//    protected void onPostExecute(String response){
+//        if(response == null){
+//            response = "THERE WAS AN ERROR";
+//        }
+//
+//        Log.i("INFO",response);
+//        responseView.setText(response);
+//    }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
---
+++ Android/SnapChef/app/src/main/java/app/snapchef/com/base/apiTESTActivity.java
@@ -0,0 +1,121 @@
+package app.snapchef.com.base;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import app.snapchef.com.base.R;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class apiTESTActivity extends AppCompatActivity {
+
+    TextView responseView;
+    ProgressBar progressBar;
+    static final String API_URL = "https://1y81ltee41.execute-api.us-east-1.amazonaws.com/default/BackendLambda";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_test);
+
+        responseView = (TextView) findViewById(R.id.responseView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        Button queryButton = (Button) findViewById(R.id.queryButton);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new RetrieveFeedTask().execute();
+            }
+        });
+    }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            responseView.setText(response);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+}
