63621c4445a2d279946dbe5f4c05647a6936e5cb
RamVasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2018-12-14 22:31:57 -0500

    Update lambda_function.py
    Co-authored-by: "Gong-Fan (Billy) Bao" <billy.bao11@gmail.com>
    BrandonBenoit <43153258+BrandonBenoit@users.noreply.github.com>

--- Backend_Code/Lambda/lambda_function.py
+++ Backend_Code/Lambda/lambda_function.py
@@ -2,8 +2,8 @@
 '''
 Created on Dec. 11, 2018
 
-@author: Brandon Benoit, Gong-Fan Bao
-@version: 2018-12-12
+@author: Brandon Benoit, Gong-Fan Bao, Sriram Vasuthevan
+@version: 2018-12-14
 '''
 
 import json
@@ -17,7 +17,6 @@ name = "backend" #host username
 passwrd = "coachrocky" #get pass
   
 def lambda_handler(event, context):
-    
     response = None;
     reponseCode = 200;
     
@@ -29,49 +28,74 @@ def lambda_handler(event, context):
     
     #Use Case statements to determine operation
     
-
-    operation = event["operation"];
+    
+    operation = event["queryStringParameters"]["operation"]
+    ingredients = event["multiValueQueryStringParameters"]["ingredient"]
     
     if (operation == "getRecipes"):
-        print ("hi")
-    elif (operation == "getIngredients"):
-        print("dab")
-
         
-    try:
-
-        with con.cursor() as cursor:
-        
-            #SQL
-            sql = "SELECT recipeName, Rating, instructions FROM SnapChef.TEST WHERE ingredientName IN ('garlic', 'salt')  GROUP BY recipeName HAVING COUNT(*) = 2"
-            #ingredientName IN ('milk',  'butter'), change milk and butter to whichever ingredients the user enters
-            #change COUNT to number of ingredients entered
+        try:
+    
+            with con.cursor() as cursor:
             
-            #Initialize Return Object
-            recipes = {}; #Empty Dictionary
+                #SQL
+                sql = "SELECT recipeName, Rating, instructions FROM SnapChef.TEST WHERE ingredientName IN "+ genIngredientsQuery(ingredients) +"  GROUP BY recipeName HAVING COUNT(*) = "+str(len(ingredients))
+                #ingredientName IN , change milk and butter to whichever ingredients the user enters
+                #change COUNT to number of ingredients entered
                 
-            #Execute Query
-            cursor.execute(sql)
-            
-            print("cursor.description: ", cursor.description)
-            print()
-            
-            #translating rows to dictionary
-            counter = 1
-            for row in cursor:
-                print(row)
+                #Initialize Return Object
+                recipes = []; 
+                    
+                #Execute Query
+                cursor.execute(sql)
                 
-                recipes.update({"recipe" + str(counter) : row[0]});
-                recipes.update({"rating" + str(counter) : row[1]});
-                recipes.update({"instructions" + str(counter) : row[2]});
-                counter += 1;
+                print("cursor.description: ", cursor.description)
+                print()
                 
-            recipes_json = {"recipes": recipes};
-            response = json.dumps(recipes_json);
-
-    finally:
-        #close connection
-        con.close();
+                #translating rows to dictionary
+                for row in cursor:
+                    print(row)
+                    newRecipes={}
+                    newRecipes["recipe"]=row[0]
+                    newRecipes["rating"]=row[1]
+                    newRecipes["instructions"]=row[2]
+                    
+                    with con.cursor() as temp_cursor:
+                        
+                        sql_getIngredients = "SELECT ingredientName FROM SnapChef.TEST WHERE recipeName = " + "\'"+newRecipes["recipe"]+"\'"
+                        temp_cursor.execute(sql_getIngredients)
+                            
+                        print("cursor.description: ", cursor.description)
+                        print()
+                        recipeIngredients=""
+                        for row in temp_cursor: 
+                            recipeIngredients += row[0]+"\n " #there is only one column
+                            
+                    newRecipes["ingredients"]=recipeIngredients
+                    
+                    recipes.append(newRecipes)
+                    
+                recipes_json = {"recipes": recipes};
+                response = {
+                    "statusCode": 200,
+                    "headers": {
+                        "my_header": "my_value"
+                    },
+                    "body": json.dumps(recipes),
+                    "isBase64Encoded": False
+                };
+    
+        finally:
+            #close connection
+            con.close();
         
     return response;
+    
+def genIngredientsQuery(Ingredients):
+    Query="("
+    for i in Ingredients:
+        Query+="\'"+i+"\',"
+    Query=Query[:-1]
+    Query+=")"
+    return Query
         
