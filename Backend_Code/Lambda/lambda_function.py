f4581ee68e0b22749b17d53c1597d5629be4d7fc
Gong-Fan (Billy) Bao <billy.bao11@gmail.com>
2018-12-14 19:15:49 -0500

    Update lambda_function.py

--- Backend_Code/Lambda/lambda_function.py
+++ Backend_Code/Lambda/lambda_function.py
@@ -1,27 +1,77 @@
+
+'''
+Created on Dec. 11, 2018
+
+@author: Brandon Benoit, Gong-Fan Bao
+@version: 2018-12-12
+'''
+
 import json
-import sys
 import pymysql
+import pymysql.cursors
 
+host_address = 'snapchefdbinstance.chvgyyoopqkx.us-east-2.rds.amazonaws.com' #endpoint
+DB_name = "snapchefdbinstance" #the db name
 
-rds_host  = "snapchefdbinstance.chvgyyoopqkx.us-east-2.rds.amazonaws.com:3306"
-name = "backend"
-password = "Snapchef2018"
-db_name = "snapchefdbinstance"
-
-
-ResCode=""
-
-try:
-    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
-    ResCode="Good"
-except Exception as e:
-    ResCode="Fail"
-    print(e)
-    sys.exit()
-
-
+name = "backend" #host username
+passwrd = "coachrocky" #get pass
+  
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps(ResCode)
-    }
+    
+    response = None;
+    reponseCode = 200;
+    
+    #simple connection
+    con = pymysql.connect(host = host_address,
+                      port = 3306,
+                      user = name,
+                      password = passwrd);
+    
+    #Use Case statements to determine operation
+    
+
+    operation = event["operation"];
+    
+    if (operation == "getRecipes"):
+        print ("hi")
+    elif (operation == "getIngredients"):
+        print("dab")
+
+        
+    try:
+
+        with con.cursor() as cursor:
+        
+            #SQL
+            sql = "SELECT recipeName, Rating, instructions FROM SnapChef.TEST WHERE ingredientName IN ('garlic', 'salt')  GROUP BY recipeName HAVING COUNT(*) = 2"
+            #ingredientName IN ('milk',  'butter'), change milk and butter to whichever ingredients the user enters
+            #change COUNT to number of ingredients entered
+            
+            #Initialize Return Object
+            recipes = {}; #Empty Dictionary
+                
+            #Execute Query
+            cursor.execute(sql)
+            
+            print("cursor.description: ", cursor.description)
+            print()
+            
+            #translating rows to dictionary
+            counter = 1
+            for row in cursor:
+                print(row)
+                
+                recipes.update({"recipe" + str(counter) : row[0]});
+                recipes.update({"rating" + str(counter) : row[1]});
+                recipes.update({"instructions" + str(counter) : row[2]});
+                counter += 1;
+                
+            recipes_json = {"recipes": recipes};
+            response = json.dumps(recipes_json);
+
+    finally:
+        #close connection
+        con.close();
+        
+    return response;
+        
